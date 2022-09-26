69cc2da6e571f8addc82ce6abddf2ca6a2539a2c
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-09-25 23:45:04 -0400

    adding controller items

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.send('<h1>404 Page Not Found</h1>')
+    res.status(404) .send('<h1>404 Page Not Found</h1>')
 })
 
 
 //app.listen(3000)
 app.listen(process.env.PORT)
+
+app.use('/places', require('./controllers/places'))
 
