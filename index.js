310f6af2a717fb47d919006a5b9c92a0700d73b8
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-09-25 23:05:50 -0400

    added 404 page route

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+//app.listen(3000)
+app.listen(process.env.PORT)
+
