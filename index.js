127f259be0cd0062875fc227d1ac9a69ca833d55
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-09-25 23:10:11 -0400

    add res.status(404)

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page Not Found</h1>')
+})
+
+
 //app.listen(3000)
 app.listen(process.env.PORT)
 
