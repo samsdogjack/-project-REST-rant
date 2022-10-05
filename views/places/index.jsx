a2328b203d9680c2712b5c731b2279d84d06da57
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-10-05 16:14:04 -0400

    restrant submission 3

---
+++ views/places/index.jsx
@@ -0,0 +1,16 @@
+const React = require("react");
+const Def = require("./default");
+
+function index() {
+  return (
+    <Def>
+      <main>
+        <h1>Index</h1>
+        <p>Welcome to the index page</p>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = index;
+
