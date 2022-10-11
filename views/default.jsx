8711e2529820bc74eae43778a225443279532a11
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-10-11 12:09:02 -0400

    adding css

--- views/default.jsx
+++ views/default.jsx
@@ -1,9 +1,11 @@
-const React = require("react");
-function Def(html) {
+const React = require("react")
+
+function Def (html) {
   return (
     <html>
       <head>
         <title>Title</title>
+        <link rel="stylesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">" />
       </head>
       <body>{html.children}</body>
     </html>
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,16 +1,22 @@
-const React = require('react')
-const Def = require("../default")
+const React = require("react");
+const Def = require("../default");
 
-function new_form () {
-    return (
-        <Def>
-            <main>
-            <h1>Add a new place</h1>
-            </main>
-        </Def>
-    )
+function new_form() {
+  return (
+    <Def>
+      <main>
+        <h1>Add a new place</h1>
+        <form method="POST" action="/places">
+          <div>
+            <label html For="name">Place Name</label>
+            <input id="name" name="name" />
+              Place Name
+              <input />{" "}
+          </div>
+        </form>
+      </main>
+    </Def>
+  );
 }
 
-
-module.exports = new_form
-
+module.exports = new_form;
